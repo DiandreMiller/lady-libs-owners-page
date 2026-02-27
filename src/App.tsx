c2e570f954237a7fb07d986a4865187fbf0ab5a5
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2026-02-27 00:46:29 -0500

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// Ask libby about design
+// Pink, Cream, and Brown 
+// Cutesy
+// 
 
 function App() {
   const [count, setCount] = useState(0)
